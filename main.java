d6a4f02a94b9edba8c660ee7dd74b7968f9772fe
nelets <165931603+nelets@users.noreply.github.com>
2024-04-03 19:19:27 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,50 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+   
+
+        // winter, spring, summer, autumn
+        // warm jacket, T-shirt, swimming suite, rain coat
+
+        
+    
+    
+    String season = "tgh";
+
+        if (season == "winter") {
+          System.out.println("Wear a warm jacket!");
+        }
+        else if (season == "spring") {
+          System.out.println("Wear a T-shirt!");
+        }
+        else if (season == "summer") {
+          System.out.println("Wear a swimming suite!");
+        }
+        else if (season == "autumn") {
+          System.out.println("Wear a rain coat!");
+        }
+    else {
+      System.out.println("I do not recognize the season!");
+    }
+    double temp = -15.0;
+
+    if (temp <= 5) {
+      System.out.println("Wear super warm");
+    }
+    else if (temp <= 15) {
+      System.out.println("Wear warm");
+    }
+    else if (temp <= 30) {
+      System.out.println("Wear normal");
+    }
+    else {
+      System.out.println("You need cooling");
+    }
+    
+        }
+
+
+    }
